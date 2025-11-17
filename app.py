3019ee710eef8e12a2eefcd46d2c78e6407932a3
pariparmar1 <mepari108@gmail.com>
2025-11-17 15:12:58 -0500

    commit code
    Added a welcome message and input handling for study hours.

--- app.py
+++ app.py
@@ -1,1 +1,16 @@
-
+#Welcome message
+print("Welcome to my Python program!")
+#Ask for input 
+hours = input("How many hours did you study today? ")
+#convert input and perform calculations
+hours = float(hours)
+weekly_hours = hours * 7
+#Display the result
+print(f"You are on track to study {weekly_hours} hours this week.")
+#Add simple error handling
+try:
+    hours = float(hours)
+except ValueError:
+    print("Please enter a valid number.")
+    exit()
+  
